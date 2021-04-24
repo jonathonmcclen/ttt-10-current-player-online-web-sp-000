126f5fb97cb12a677233f3da8021beabab88f56f
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-24 17:53:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,20 @@
  
- def turn_count(boards)
+def turn_count(board)
+  number_turns = 0
   board.each |box| do
     if box == "X" || box == "O"
-      
+      number_turns += 
     end
+    return number_turns
+  end
+end 
+ 
+def current_player(board)
+  if turn_count == 0
     
+  elsif turn_count % 2 == 0
+  
+  else
     
   end
-   number_turns +=1
-   
- end 
- 
- def current_player
-   
- end
+end
